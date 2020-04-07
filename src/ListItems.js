21e81c8b0f2a2276471cc4e66cf067e1c7e56257
iqbalptr <miqbalperdanaputra404@gmail.com>
2020-04-08 06:38:58 +0700

    fix

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './components/ListItems.css'
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import FlipMove from 'react-flip-move';
 
+
+
+
 function ListItems(props){
+
     const items = props.items;
     const listItems = items.map(item =>
+
    {
        return <div className="list" key={item.key}>
      <p>
+         
          <input type="text" id={item.key} value={item.text} onChange={(e)=>{
              props.setUpdate(e.target.value,item.key)}}/>
         <span>
@@ -18,15 +23,20 @@ function ListItems(props){
             props.deleteItem(item.key)
         }} icon="trash" />
         </span>
+
      </p>
      
     </div>})
-    return <div>
+
+    return (
+    <div>
+       
         <FlipMove duration={300} easing="ease-in-out">
         {listItems}
         </FlipMove>
     
-    </div>;
+    </div>)
+
   }
 
   export default ListItems;
